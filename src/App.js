d722429f89a66f4687b556238150886a29226038
Byung Yoo <byung@Byungs-MacBook-Pro.local>
2020-08-03 16:43:29 -0400

    Updates as of Aug 3

--- src/App.js
+++ src/App.js
@@ -38,6 +38,11 @@ function App() {
 //  you can think of useEffect Hook as componentDidMount, componentDidUpdate, and componentWillUnmount combined
 
 
+  const [ipAddr, setIPAddr] = useState();
+
+
+
+
 
 
   useEffect(() => {
@@ -160,10 +165,77 @@ useEffect(() => {
         });
 
   }
+  else{
+
+
+  }
 
 },[]);
 
 
+// get ip addr
+useEffect(() => {
+
+  // only if current location is not enabled
+  if(!navigator.geolocation){
+
+  }
+
+  fetch(`https://ipapi.co/json`)
+    .then(res => res.json())
+    .then(
+        (result) => {
+          console.log("IP Addr:")
+          console.log(result.ip)
+        console.log(result.longitude)
+        console.log(result.latitude)
+      //  setIPAddr(result.ipString)
+
+      setUrl(`https://www.google.com/maps/embed/v1/view?key=${apiKey}&center=${result.latitude},${result.longitude}&zoom=10`);
+      console.log(`setUrlll - using ip: ${url}`)
+
+        },
+
+        (error) => {
+
+
+        }
+      )
+
+
+
+
+
+
+
+
+
+}, []);
+/*
+useEffect(() => {
+
+  fetch(`http://api.ipstack.com/${ipAddr}?access_key=14182789d54e3b723358831c92068d64`)
+    .then(res => res.json())
+    .then(
+        (result) => {
+          console.log("lat/long based on IP:")
+        console.log(result.latitude + ", " + result.longitude)
+
+        setUrl(`https://www.google.com/maps/embed/v1/view?key=${apiKey}&q=center=${result.latitude},${result.longitude}&zoom=${10}`);
+        console.log(`setUrlll - using ip: ${url}`)
+
+
+        },
+
+        (error) => {
+
+
+        }
+      )
+
+
+}, [ipAddr]);
+*/
 
   if ("geolocation" in navigator) {
     console.log("Available");
@@ -187,74 +259,175 @@ const setDestination = () => {
 
     //setDest({ latitude: 43.649051, longitude: -79.381694 })
 
-    const dest = 'ChIJf3Z9pdI0K4gRMfvVn-uyWpM'
-    setDest(dest)
+    //const dest = 'ChIJf3Z9pdI0K4gRMfvVn-uyWpM'
+    //setDest(dest)
 
-    console.log(`setDestination: ${destination}`)
+    //console.log(`setDestination: ${destination}`)
 
     // better to use dest insteadd of the state variable,
     // b/c it seems to be asynchronous... meaning it can sometimes be undefined
     // (state being assigned after these if cases)
   //  if(dest !== undefined ){
 
+
+
     console.log("origin:::")
     console.log(origin.addr)
     console.log(origin.lat)
 
-      // precedence for origin  :
-      // place ID -> address -> lat/lng
-      if(origin !== undefined && origin.placeID !== undefined && origin.placeID != ''){
-        setUrl(
-            `https://www.google.com/maps/embed/v1/directions`
-                + `?key=${apiKey}`
-                + `&origin=place_id:${origin.placeID}`
-                + `&destination=place_id:${dest}`
-                + `&mode=walking`
+    var dest = '';
+    const proxyurl = "https://cors-anywhere.herokuapp.com/"
 
-              )
-        }
-        else if(origin !== undefined && origin.addr !== undefined && origin.addr !== ''){
-          setUrl(
-              `https://www.google.com/maps/embed/v1/directions`
-                  + `?key=${apiKey}`
-                  + `&origin=${origin.addr}`
-                  + `&destination=place_id:${dest}`
-                  + `&mode=walking`
+    fetch(proxyurl + `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${origin.lat},${origin.lng}&radius=500&opennow=true&type=restaurant&key=${apiKey}`)
+      .then(res => res.json())
+      .then(
+          (result) => {
+      //      setIsLoaded(true);
+        //    setItems(result.items);
+        console.log("JSON Result - nearby places: ")
+          console.log(result)
+          //placeID = result.results[0].place_id;
 
-                )
-        }
-        else if(origin !== undefined && origin.lat !== undefined && origin.lng !== undefined ){
-            setUrl(
-                `https://www.google.com/maps/embed/v1/directions`
-                    + `?key=${apiKey}`
-                    + `&origin=${origin.lat},${origin.lng}`
-                    + `&destination=place_id:${dest}`
-            //        + `&destination=${destination.latitude},${destination.longitude}`
-                    + `&mode=walking`
 
-                  )
 
-        }
-        else{
-            // send warning - origin not detected
-            console.log("Origin not detected")
-          // freeze the Go button if both origin and originPlaceID are not available yet?
+          // note: origin.addr is not updated here yet (async)
 
-        }
+
+          //setUrl(`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${addr}&zoom=${defaultZoom}`);
+
+          const count = result.results.length
+          console.log(`count: ${count}`)
+
+          var random = Math.floor(Math.random() * Math.floor(count+1));
+
+          console.log(`random: ${random}`)
+
+          dest = result.results[random];
+
+
+            if(result.status == "ZERO_RESULTS"){
+              console.log("No results")
+            }
+            else{
+              if(dest !== undefined && dest.place_id !== ''){
+                  setDest(dest)
+                  console.log(`Destination: ${dest}`)
+                  setDestUrl(dest)
+
+                      //returnDestDetails(dest)
+
+              }
+
+            }
+          },
+          // Note: it's important to handle errors here
+          // instead of a catch() block so that we don't swallow
+          // exceptions from actual bugs in components.
+          (error) => {
+          //  setIsLoaded(true);
+        //    setError(error);
+
+          // if address/place id is not retrieved, just use the lat/lng
+
+        //  setUrl(`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${origin.lat},${origin.lng}&zoom=${defaultZoom}`);
+
+          }
+        )
+
+
+
 
 
 
 }
 
+const setDestUrl = (destObject) => {
+
+  // precedence for origin  :
+  // place ID -> address -> lat/lng
+
+
+
+  const dest = destObject.place_id;
+
+  if(origin !== undefined && origin.placeID !== undefined && origin.placeID != ''){
+    setUrl(
+        `https://www.google.com/maps/embed/v1/directions`
+            + `?key=${apiKey}`
+            + `&origin=place_id:${origin.placeID}`
+            + `&destination=place_id:${dest}`
+            + `&mode=walking`
+
+          )
+
+
+    }
+    else if(origin !== undefined && origin.addr !== undefined && origin.addr !== ''){
+      setUrl(
+          `https://www.google.com/maps/embed/v1/directions`
+              + `?key=${apiKey}`
+              + `&origin=${origin.addr}`
+              + `&destination=place_id:${dest}`
+              + `&mode=walking`
+
+            )
+    }
+    else if(origin !== undefined && origin.lat !== undefined && origin.lng !== undefined ){
+        setUrl(
+            `https://www.google.com/maps/embed/v1/directions`
+                + `?key=${apiKey}`
+                + `&origin=${origin.lat},${origin.lng}`
+                + `&destination=place_id:${dest}`
+        //        + `&destination=${destination.latitude},${destination.longitude}`
+                + `&mode=walking`
+
+              )
+
+    }
+    else{
+        // send warning - origin not detected
+        console.log("Origin not detected")
+      // freeze the Go button if both origin and originPlaceID are not available yet?
+
+    }
+
+
+
+}
+
+const returnDestDetails = () =>{
+
+  if( destination === undefined){
+    return null
+  }
+
+
+
+  return(
+
+
+<div style={{marginLeft:'auto', marginRight:'auto', textAlign:'center'}}>
+    <div id="destTitle">Destination:</div>
+    <div id="destDetails">
+        {destination.name} <br />
+
+        <a href={`https://www.google.com/maps/place/?q=place_id:${destination.place_id}`} target="_blank">View Details</a>
+
+
+
+    </div>
+  </div>
+  )
+}
+
+
+
+
 const getIPAddr = () =>{
 
-  var http = require('http');
-  http.get('http://bot.whatismyipaddress.com', function(res){
-      res.setEncoding('utf8');
-      res.on('data', function(chunk){
-          console.log(chunk);
-      });
-  });
+
+
+
 
 }
 
@@ -290,9 +463,13 @@ const getIPAddr = () =>{
 }
       </div>
       origin: {origin.lat}, {origin.lng} <br />
-      IP: {getIPAddr()}
       <br />
-      <br />
+
+      <div id="dest">
+
+        {returnDestDetails()}
+
+      </div>
         <div>
           <button type="button" onClick={setDestination}>GOO</button>
         </div>
